d1cb60cb432fa223dab5cff74f84cb64cbf9653b
zile <cfzile@gmail.com>
2021-10-23 02:10:33 +0300

    some change

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include "general.h"
 #include <functional>
+#include <sys/resource.h>
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#pragma comment(lib, "version.lib")
+//#pragma comment(lib, "libversion.a")
+//#pragma comment(linker, "/stack:200000000")
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+//#pragma GCC optimization ("O3")
+//#pragma GCC optimization ("unroll-loops")
+
 
 struct MAX_CLIQUE {
     int size = -1;
@@ -14,12 +25,17 @@ public:
     int largest_independent_set_size = 1000;
     int min_independent_set_size = 3;
     uniform_int_distribution<int> random_int;
+    IloEnv env;
+    IloModel model;
+    IloFloatVarArray x;
 
     explicit MaxCliqueSolver(const GRAPH &graph) {
         this->graph = graph;
         this->number_vertices = graph.number_vertices;
         random_int = uniform_int_distribution<int>(0, number_vertices - 1);
-
+        model = IloModel(env);
+        x = IloFloatVarArray(env, number_vertices);
+        create_problem(find_sets());
     }
 
     void find_set(int deep, set<int> cur_set, vector<int> &used, set<set<int>> &sets, map<EDGE, bool> &pairs) {
@@ -29,7 +45,7 @@ public:
         auto i = used.begin();
         while (i != used.end() && (int) cur_set.size() < largest_independent_set_size) {
             bool independent = true;
-            for (auto &j : cur_set)
+            for (auto &j: cur_set)
                 if (graph.graph_matrix[min(*i, j)][max(*i, j)]) {
                     independent = false;
                     break;
@@ -47,8 +63,8 @@ public:
         }
 
         if (deep >= min_independent_set_size && (deep == largest_independent_set_size || !found_large)) {
-            for (auto &i : cur_set)
-                for (auto &j : cur_set)
+            for (auto &i: cur_set)
+                for (auto &j: cur_set)
                     if (pairs.find({min(i, j), max(i, j)}) != pairs.end())
                         pairs.erase({min(i, j), max(i, j)});
             sets.insert(cur_set);
@@ -70,7 +86,7 @@ public:
         cout << "No edges: " << pairs.size() << "\n";
 
         for (int i = 0; i < number_vertices; ++i) {
-            for (int _ = 0; _ < 1; ++_){
+            for (int _ = 0; _ < 1; ++_) {
                 vector<int> used;
                 for (int j = 0; j < number_vertices; ++j) {
                     int add = random_int(rng) % number_vertices;
@@ -83,23 +99,34 @@ public:
 
         cout << "Independent sets size 2: " << pairs.size() << "\n";
 
-        for (auto &i : pairs)
+        for (auto &i: pairs)
             sets.insert(set<int>({i.first.first, i.first.second}));
 
         cout << "Found independent sets (+ size 2): " << sets.size() << "\n";
         return sets;
     }
 
-    MAX_CLIQUE solve() {
-        IloEnv env;
-        IloModel model(env);
+    double solve() {
+        IloCplex cplex(model);
+        cplex.exportModel("model.lp");
+        if (cplex.solve()) {
+            auto xx = x;
+            sort(bounds.begin(), bounds.end(), [&cplex, &xx](int a, int b) {
+                return cplex.getValue(xx[a]) < cplex.getValue(xx[b]);
+            });
+            return cplex.getObjValue();
+        }
+        return -1;
+    }
+
+
+    void create_problem(set<set<int>> sets) {
 
         std::stringstream name;
-        IloBoolVarArray x(env, number_vertices);
 
         for (auto i = 0; i < number_vertices; ++i) {
             name << "x_" << i;
-            x[i] = IloBoolVar(env, 0, 1, name.str().c_str());
+            x[i] = IloFloatVar(env, 0., 1., name.str().c_str());
             name.str("");
         }
 
@@ -107,18 +134,17 @@ public:
 
         IloExpr expr(env);
 
-        set<set<int>> sets = find_sets();
-        for (const auto &set : sets) {
-            for (auto &i : set)
+        for (const auto &set: sets) {
+            for (auto &i: set)
                 expr += x[i];
-            independent_sets.add(IloRange(env, 0, expr, 1));
+            independent_sets.add(IloRange(env, 0., expr, 1.));
             expr.clear();
         }
 
-        model.add(independent_sets);
-
         for (auto i = 0; i < number_vertices; ++i)
             expr += x[i];
+
+        model.add(independent_sets);
 
         IloObjective obj(env, expr, IloObjective::Maximize);
 
@@ -126,38 +152,124 @@ public:
 
         expr.end();
 
-        IloCplex cplex(model);
-        cplex.exportModel("model.lp");
-
-        bool solved = false;
-
-        try {
-            solved = cplex.solve();
-        } catch (const IloException &e) {
-            std::cerr << "\n\nCPLEX Raised an exception:\n";
-            std::cerr << e << "\n";
-            env.end();
-            throw;
-        }
-
-        if (solved) {
-            vector<int> vertices;
-            for (int i = 0; i < number_vertices; i++) {
-                if (cplex.getValue(x[i]) >= 1 - EPS)
-                    vertices.push_back(i + 1);
-            }
-            return MAX_CLIQUE{(int) cplex.getObjValue(), vertices};
-        }
-
-        env.end();
-
-        return MAX_CLIQUE{};
     }
+
+    MAX_CLIQUE BnB() {
+        bounds.reserve(number_vertices);
+        for (int i = 0; i < number_vertices; i++)
+            bounds.push_back(i);
+
+        vector<int> v(number_vertices, 0);
+        for (int i = 0; i < number_vertices; i++)
+            for (int j = 0; j < number_vertices; j++)
+                v[i] += graph.graph_matrix[i][j];
+
+        sort(bounds.begin(), bounds.end(), [&v](int a, int b) {
+            return v[a] < v[b];
+        });
+        BnB_();
+        return gbAns;
+    }
+
+    MAX_CLIQUE gbAns;
+    int globalAns = -1;
+    vector<int> solution;
+    vector<int> bounds;
+    int v = -1, value = 0;
+
+    void BnB_() {
+
+        if ((int) bounds.size() + (int) solution.size() <= globalAns)
+            return;
+
+        bool b = value == 1;
+        cout << v << " " << globalAns << " gb\n";
+        IloRange constraint;
+        IloExpr expr(env);
+
+        if (v != -1) {
+            if (b)
+                solution.push_back(v);
+
+            expr = x[v];
+            constraint = IloRange(env, value, expr, value);
+            model.add(constraint);
+
+            double sol = solve();
+
+            cout << "sols " << sol << " " << solution.size() << "\n";
+
+            if (floor(sol) == solution.size()) {
+                if (globalAns < sol) {
+                    globalAns = floor(sol);
+                    gbAns = MAX_CLIQUE{(int) floor(sol), solution};
+                }
+            }
+
+            if (floor(sol) <= globalAns) {
+                expr.end();
+                model.remove(constraint);
+                if (b)
+                    solution.pop_back();
+                return;
+            }
+        }
+
+        if (bounds.empty()) {
+            globalAns = solution.size();
+            gbAns = MAX_CLIQUE{(int) solution.size(), solution};
+            model.remove(constraint);
+            solution[v] = -1;
+            return;
+        }
+
+        int vertex = bounds.back();
+        bounds.pop_back();
+
+        bool add = true;
+        for (int i: solution)
+            if (graph.graph_matrix[vertex][i] == 0)
+                add = false;
+
+        if (add) {
+            v = vertex, value = 1;
+            BnB_();
+        }
+
+        v = vertex, value = 0;
+        BnB_();
+
+        bounds.push_back(vertex);
+
+        if (v != -1) {
+            model.remove(constraint);
+            if (b)
+                solution.pop_back();
+        }
+    }
+
 };
 
 int main() {
-
+//    {
+//        const rlim_t kStackSize = 1024 * 1024 * 1024;   // min stack size = 16 MB
+//        struct rlimit rl;
+//        int result;
+//
+//        result = getrlimit(RLIMIT_STACK, &rl);
+//        if (result == 0) {
+//            if (rl.rlim_cur < kStackSize) {
+//                rl.rlim_cur = kStackSize;
+//                result = setrlimit(RLIMIT_STACK, &rl);
+//                if (result != 0) {
+//                    cout << "setrlimit returned result = " <<  result;
+//                }
+//            }
+//        }
+//
+//    }
     auto info = read_info();
+
     ofstream result("../result.csv");
 
     for (auto &graph_case : info) {
@@ -168,12 +280,12 @@ int main() {
         MaxCliqueSolver solver = MaxCliqueSolver(graph);
 
         auto begin = chrono::system_clock::now();
-        MAX_CLIQUE ans = solver.solve();
+        MAX_CLIQUE ans = solver.BnB();
         auto time = getDiff(begin);
 
         cout << time << " ";
 
-        assert(ans.vertices.size() == ans.size);
+//        assert(ans.vertices.size() == ans.size);
 
         if (ans.size == graph_case.second) {
             result << graph_case.first << ",";
@@ -181,6 +293,7 @@ int main() {
             cout << "Accepted\n";
         } else {
             cout << ans.size << " " << graph_case.second << " : Error\n";
+            throw;
         }
 
     }
